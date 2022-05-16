af49de3ea7a5efec28bb5e461d5dbf5b684cc4be
Samkim <94517523+freloha@users.noreply.github.com>
2022-05-16 14:29:39 +0900

    Update simpleserver.cpp

--- simpleserver.cpp
+++ simpleserver.cpp
@@ -38,15 +38,18 @@ int main()
     memset(&servAddr, 0, sizeof(servAddr));
 
     //AF_INET : 인터넷 프로토콜 IPv4를 의미
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(30002);
+    servAddr.sin_family = AF_INET; // IPv4 사용
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = 자동으로 이 컴퓨터에 존재하는 랜카드 중 사용가능한 랜카드의 IP 주소를 사용
+    servAddr.sin_port = htons(30002); // 데이터를 네트워크 바이트 순서로 변환
 
     //servAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 이 함수가 아래의 양식으로 바뀌었음
     //inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr);
 
-    if (bind(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    // bind(소켓 변수, 서버 주소 구조체, 서버 주소 구조체의 크기)
+    if (bind(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) // bind : 소켓에 주소, 프로토콜, 포트를 할당
         printf("Binding Error \n");
+    
+    // listen(소켓 변수, 백 로그 큐의 크기) : 클라이언트로부터 연결 요청을 기다림
     if (listen(hSocket, 5) == SOCKET_ERROR)
         printf("listen Error\n");
 
@@ -54,6 +57,8 @@ int main()
     SOCKADDR_IN clientAddr;
     SOCKET clientSocket;
     int sizeClientAddr = sizeof(clientAddr);
+    
+    // accept(소켓 변수, 클라이언트 주소 구조체 변수, 클라이언트 주소 구조체 크기) : 서버 측에서 클라이언트 연결을 수락, 오류 발생시 -1 반환
     clientSocket = accept(hSocket, (SOCKADDR*)&clientAddr, &sizeClientAddr);
     if (clientSocket == SOCKET_ERROR)
         printf("Failed accept() \n");
